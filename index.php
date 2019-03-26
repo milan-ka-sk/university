626f20cecc896fc22d038571ddb7b0f64069ba1e
milan <milankoniar@gmail.com>
2019-03-26 21:51:24 +0100

    initial minimalistic setup of the theme

---
+++ index.php
@@ -0,0 +1,5 @@
+To create a theme u only need 2 files: <br>
+index.php <br>
+styles.css <br>
+Otherwise, u can adjust theme info like here https://codex.wordpress.org/Theme_Development#Theme_Stylesheet <br>
+and u can use a screenshot.png for the screenshot
