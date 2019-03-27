56b5db350b9cb7c3e75bcf005c48c0b1b954f38c
milan <milankoniar@gmail.com>
2019-03-27 09:47:46 +0100

    filled static html into page.php and set up basic wp-loop, setup TITLE for each page dynamically in functions.php, setup some nav links

--- functions.php
+++ functions.php
@@ -6,4 +6,9 @@
         wp_enqueue_style('main_styles', get_stylesheet_uri());
     }
     add_action('wp_enqueue_scripts', 'university_files');
+
+    function university_features (){
+        add_theme_support('title-tag');
+    }
+    add_action('after_setup_theme', 'university_features');
 ?>
