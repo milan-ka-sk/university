62435f3e2de65f97536a79d641226af5d7538d9f
milan <milankoniar@gmail.com>
2019-03-27 17:06:44 +0100

    setting up bradcrumbs in page.php

--- page.php
+++ page.php
@@ -17,9 +17,15 @@
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+  <?php 
+  $theParentID = wp_get_post_parent_ID(get_the_ID());
+/* if parent id = 0, there is no parent*/
+  if (wp_get_post_parent_ID(get_the_ID())) {?>
+     <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentID) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParentID) ?></a> <span class="metabox__main"><?php the_title() ?></span></p>
     </div>
+    <?php   }
+?>
     
     <!-- <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
