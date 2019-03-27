c04a25734dac71e45239cd050830e800d5ba1213
milan <milankoniar@gmail.com>
2019-03-27 17:50:59 +0100

    setup the submenu in page.php

--- page.php
+++ page.php
@@ -21,19 +21,48 @@
   $theParentID = wp_get_post_parent_ID(get_the_ID());
 /* if parent id = 0, there is no parent*/
   if (wp_get_post_parent_ID(get_the_ID())) {?>
-     <div class="metabox metabox--position-up metabox--with-home-link">
+    <div class="metabox metabox--position-up metabox--with-home-link">
       <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentID) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParentID) ?></a> <span class="metabox__main"><?php the_title() ?></span></p>
     </div>
     <?php   }
 ?>
-    
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?php
+    // if there are no pages (falsy value) this page is not a parent
+    $test = get_pages(array(
+      'child_of' => get_the_ID()
+    ));
+
+    // only show if the current page is a parent or a child
+    // dont show if the page is neither a parent nor a child
+  if($theParentID or $test){ 
+    ?>
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParentID) ?>"><?php echo get_the_title($theParentID) ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <!-- <li class="current_page_item"><a href="#">Our History</a></li>
+        <li><a href="#">Our Goals</a></li> -->
+
+        <?php 
+          if($theParentID){
+            //this page has a parent -- the site is a child
+            $motherID =  $theParentID;
+          } else{
+            //this page has no parent -- it is top level
+            $motherID =  get_the_ID();
+          }
+
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => $motherID,
+            'sort_column' =>  'menu_order' // 
+          ));
+        ?>
       </ul>
-    </div> -->
+    </div>  
+
+    <?php
+   }
+    ?>
 
     <div class="generic-content">
     <?php the_content() ?>
